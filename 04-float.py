c67cbd1ea6ffd20e96005feaa5c04aa86bca9be9
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-07-25 15:28:49 +0500

    Create 04-float.py

---
+++ 04-float.py
@@ -0,0 +1,8 @@
+x = float(input("What's x?"))
+y = float(input("What's y?"))
+
+#input always takes string
+z = round(x + y)
+print(f"{z:,}") #to represent , after 1
+
+print(float(input("What's x?")) + float(input("What's y?")))
