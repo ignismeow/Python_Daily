43ec38efadbfd329a5458d9b9cbd0f4cdd2e9107
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-07-26 14:27:01 +0500

    Update 06-def.py

--- 06-def.py
+++ 06-def.py
@@ -1,3 +1,4 @@
+#01 - Another function is 
 def main():
     name = input("What's your name? ")
     hello(name)
@@ -6,3 +7,18 @@ def hello(to="World"):   #def hello(to="saad"):
     print('Hello,', to.strip().title())
     
 main()
+
+#02 - Another function is 
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+    
+def square(n):
+    return n * n
+    #other two ways to get exponent are 
+    return n**2
+    return pow(n,2)
+
+main()
+
