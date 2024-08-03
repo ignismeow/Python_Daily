2f3376c03042b4c094407b3eb6bb9b72880d1963
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-08-04 01:28:53 +0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -21,3 +21,32 @@ for name in names: # other way
 Vehicles = ["Rubicon", "Ferrari", "Rolls Royce", "lamborgini"]
 for vehicle in Vehicles:
     print(f"My second priority is {vehicle}")
+
+#Problem 3: Store names of some friends in list called name, print each name once at a time, send each of them personalized message, make a list of your favourite transportation vehicles and make a statment using this list items
+names = ["Asad", "Ali", "Umer", "Agha"]
+print(names[0]) #one way
+print(names[1])
+print(names[2])
+print(names[3])
+
+#Problem 3.1: Change the list of guests name
+names.remove("Ali")
+names.remove("Agha")
+names.insert(1, "Saad")
+names.insert(3, "Fawad")
+print(f"{names.pop()} Thanks for your presence I really appreciate that.")
+del names[2]
+print(names)
+
+for name in names: # other way
+    print(f"{name} you are really close to me I really like to hang out with you I want you to spare some precious time and come to my event, Please!")
+
+#Problem 3.2:
+Vehicles = ["Rubicon", "Ferrari", "Rolls Royce", "lamborgini"]
+del Vehicles[3]
+Vehicles.insert(1,"Fortuner")
+Vehicles.pop(1)
+Vehicles.remove('Rubicon')
+print(Vehicles)
+for vehicle in Vehicles:
+    print(f"My second priority is {vehicle}.")
