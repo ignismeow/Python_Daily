5b56d34ed520e5213017880358e7e62c96e4d5cf
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-08-07 11:53:20 +0500

    Update day5.py

--- day5.py
+++ day5.py
@@ -7,14 +7,32 @@ europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
 
 # Print out the capital of France
 print(europe['france']['capital'])
-
 # Create sub-dictionary data
 data = {
     'capital': 'rome', 'population': 59.83
 }
-
 # Add data to europe under key 'italy'
 europe['italy'] = data
-
 # Print europe
 print(europe)
+
+
+#Problem: Compare boolean operators
+# Comparison of booleans
+print(True == False)
+# Comparison of integers
+print((-5*15) != 75)
+# Comparison of strings
+print('pyscript' == 'PyScript')
+# Compare a boolean with an integer
+print(True == 1)
+
+#Problem: Solve comparison operators
+my_kitchen = 18.0
+your_kitchen = 14.0
+# my_kitchen bigger than 10 and smaller than 18?
+print(my_kitchen > 10 and my_kitchen < 18)
+# my_kitchen smaller than 14 or bigger than 17?
+print(my_kitchen < 14 or my_kitchen > 17)
+# Double my_kitchen smaller than triple your_kitchen?
+print(2*my_kitchen < 3*your_kitchen)
