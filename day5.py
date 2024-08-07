7b4216863ea546fc286ed9a407240f0f88292f53
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-08-07 20:25:31 +0500

    Update day5.py

--- day5.py
+++ day5.py
@@ -165,3 +165,15 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Code the for loop
 for i in areas:
     print(i)
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+# Change for loop to use enumerate() and update print()
+for x, y in enumerate(areas):
+    print(f"room {x}: {y}")
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+# Code the for loop
+for index, area in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
