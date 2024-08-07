af43a73ae0f81d14a92cd446642092aaba4b0442
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-08-07 20:09:45 +0500

    Update day5-simplecalculator.py

--- day5-simplecalculator.py
+++ day5-simplecalculator.py
@@ -1,51 +1,72 @@
-# Problem: Making a simple calculator
-def sum(num1, num2):
-    result =  num1 + num2
-    print(f"Adding {num1} with {num2} gives {result}")
-    
-def subtraction(num1, num2):
-    result =  num1 - num2
-    print(f"Substracting {num1} with {num2} gives {result}")
-    
-def multiplication(num1, num2):
-    result =  num1 * num2
-    print(f"Multiplying {num1} with {num2} gives {result}")
-    
-def division(num1, num2):
-    if num2 == 0:
-        print("Error: Division by zero is not allowed.")
-    else:
-        result =  num1 / num2
-        print(f"Dividing {num1} with {num2} gives {result}")
+import tkinter as tk
 
-def main():
-    while True:
-        print("\nSimple Calculator")
-        print("1. Add")
-        print("2. Subract")
-        print("3. Multiply")
-        print("4. Divide")
-        print("5. Exit")
+calculation = ""
+
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, 'end')
+    text_result.insert(1.0, calculation)
+    
+def evaluate_calculation():
+    global calculation
+    try:
+        calculation = str(eval(calculation))
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+    except Exception as e:
+        clear_field()
+        text_result.insert(1.0, "Error")
         
-        choice = input("Enter your choice(1-5): ")
-        
-        if choice == '5':
-            print("Exiting the calculator Goodbye!")
-            quit()
-            
-        num1 = float(input('Please enter first number: '))
-        num2 = float(input('Please enter Second number: '))
-        
-        if choice == "1":
-            sum(num1, num2)
-        elif choice == "2":
-            subtraction(num1, num2)
-        elif choice == "3":
-            multiplication(num1, num2)
-        elif choice == "4":
-            division(num1, num2)
-        else:
-            print("Invalid number please enter a valid number.")
-            
-if __name__ == "__main__":
-    main()
+    
+def clear_field():
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
+
+
+window = tk.Tk()
+window.title("Calculator")
+window.geometry("300x300")
+
+text_result = tk.Text(window, height=2, width=16, font=('Arial', 24))
+text_result.grid(columnspan=5, padx=5, pady=5)
+
+btn_1 = tk.Button(window, text="1", command=lambda: add_to_calculation(1), width=5, font=('Arial', 14))
+btn_1.grid(row=2, column=1)
+btn_2 = tk.Button(window, text="2", command=lambda: add_to_calculation(2), width=5, font=('Arial', 14))
+btn_2.grid(row=2, column=2)
+btn_3 = tk.Button(window, text="3", command=lambda: add_to_calculation(3), width=5, font=('Arial', 14))
+btn_3.grid(row=2, column=3)
+btn_4 = tk.Button(window, text="4", command=lambda: add_to_calculation(4), width=5, font=('Arial', 14))
+btn_4.grid(row=3, column=1)
+btn_5 = tk.Button(window, text="5", command=lambda: add_to_calculation(5), width=5, font=('Arial', 14))
+btn_5.grid(row=3, column=2)
+btn_6 = tk.Button(window, text="6", command=lambda: add_to_calculation(6), width=5, font=('Arial', 14))
+btn_6.grid(row=3, column=3)
+btn_7 = tk.Button(window, text="7", command=lambda: add_to_calculation(7), width=5, font=('Arial', 14))
+btn_7.grid(row=4, column=1)
+btn_8 = tk.Button(window, text="8", command=lambda: add_to_calculation(8), width=5, font=('Arial', 14))
+btn_8.grid(row=4, column=2)
+btn_9 = tk.Button(window, text="9", command=lambda: add_to_calculation(9), width=5, font=('Arial', 14))
+btn_9.grid(row=4, column=3)
+btn_0 = tk.Button(window, text="0", command=lambda: add_to_calculation(0), width=5, font=('Arial', 14))
+btn_0.grid(row=5, column=2)
+btn_plus = tk.Button(window, text="+", command=lambda: add_to_calculation("+"), width=5, font=('Arial', 14))
+btn_plus.grid(row=2, column=4)
+btn_minus = tk.Button(window, text="-", command=lambda: add_to_calculation("-"), width=5, font=('Arial', 14))
+btn_minus.grid(row=3, column=4)
+btn_multiply = tk.Button(window, text="*", command=lambda: add_to_calculation("*"), width=5, font=('Arial', 14))
+btn_multiply.grid(row=4, column=4)
+btn_divide = tk.Button(window, text="/", command=lambda: add_to_calculation("/"), width=5, font=('Arial', 14))
+btn_divide.grid(row=5, column=4)
+btn_leftpara = tk.Button(window, text="(", command=lambda: add_to_calculation("("), width=5, font=('Arial', 14))
+btn_leftpara.grid(row=5, column=1)
+btn_rightpara = tk.Button(window, text=")", command=lambda: add_to_calculation(")"), width=5, font=('Arial', 14))
+btn_rightpara.grid(row=5, column=3)
+btn_equals = tk.Button(window, text="=", command=evaluate_calculation, width=5, font=('Arial', 14))
+btn_equals.grid(row=6, column=2)
+btn_clear= tk.Button(window, text="C", command=clear_field, width=5, font=('Arial', 14))
+btn_clear.grid(row=6, column=3)
+
+window.mainloop()
