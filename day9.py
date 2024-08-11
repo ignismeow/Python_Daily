93154180ff2a69aee70a6755e84022d44fbef18c
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-08-11 23:48:05 +0500

    Update day9.py

--- day9.py
+++ day9.py
@@ -44,3 +44,19 @@ def register_user(name, email, password):
         return user
     
 register_user('ali', 'alisageneda@gmail.com', 'password123')
+
+
+# Problem: 3 iterators and iterables
+# Create a list of strings: flash
+flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+# Print each list item in flash using a for loop
+for person in flash:
+    print(person)
+# Create an iterator for flash: superhero
+superhero = iter(flash)
+# Print each item from the iterator
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+
