a59b6b507a5cabbb84d0c52b715401e32cb4a880
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-08-13 12:29:45 +0500

    Update day10.py

--- day10.py
+++ day10.py
@@ -36,3 +36,15 @@ def count_entries(csv_file, c_size, colname):
 result_counts = count_entries('tweets.csv', 10, 'lang')
 # Print result_counts
 print(result_counts)
+
+
+# Create list comprehension: squares
+squares = [i**2 for i in range(0, 10)]
+print(squares)
+
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+# Print the matrix
+for row in matrix:
+    print(row)
